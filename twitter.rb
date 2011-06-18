144c995b5cd24e5382bd9ba4d3c7d43cf5e5443a
Johannes Opper <xijo@gmx.de>
2011-06-18 19:39:03 +0200

    Default parameters are possible now.

--- twitter.rb
+++ twitter.rb
@@ -7,7 +7,8 @@ class Twitter
   include Orthrus
   remote_defaults :on_success => lambda { |response| JSON.parse(response.body) },
                   :on_failure => lambda { |response| puts "error code: #{response.code}"; {} },
-                  :base_uri   => "http://api.twitter.com"
+                  :base_uri   => "http://api.twitter.com",
+                  :version    => 1
 
   define_remote_method :search, :path   => "/:version/search.json"
   define_remote_method :trends, :path   => "/:version/trends/:time_frame.json"
@@ -16,10 +17,10 @@ class Twitter
 end
 
 # Get all tweets mentioning pluto
-tweets = Twitter.search(:version => 1, :params => {:q => "pluto"})
+tweets = Twitter.search(:params => {:q => "pluto"})
 
 # Get all current trends
-trends = Twitter.trends(:version => 1, :time_frame => :current)
+trends = Twitter.trends(:time_frame => :current)
 
 # Submit a tweet. Authentication skipped in example.
 # Twitter.tweet(:version => 1, :params => {:status => "I #love #planets! :)"})
